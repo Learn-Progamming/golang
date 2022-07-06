a7e55db7bb88db20bae201c1d47f147397139450
cristian <stevanusc00@gmail.com>
2022-07-06 16:56:20 +0700

    new code

---
+++ Golang Dasar/datastring.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main (){
+	fmt.Println("Cristian")
+	fmt.Println(len("Cristian"))
+	fmt.Println("\nCristian Stevanus")
+	fmt.Println("Cristian Stevanus"[0])
+	fmt.Println("\nCristian Stevanus Oloan")
+	fmt.Println("Cristian Stevanus Oloan"[1])
+}
---
+++ Golang Dasar/datavariable.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main (){
+	var name string
+
+	name = "Cristian Stevanus"
+	fmt.Println(name)
+
+	name = "Cristian Oloan"
+	fmt.Println(name)
+
+	var nama = "Cristian Sianturi"
+	fmt.Println(nama)
+
+	var age = 22
+	fmt.Println(age)
+
+	var age_new int8 = 22
+	fmt.Println(age_new)
+
+	baju:= "Kemeja"
+	fmt.Println(baju)
+
+	var (
+		firstName = "Cristian"
+		lastName = "Stevanus"
+	)
+	
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
