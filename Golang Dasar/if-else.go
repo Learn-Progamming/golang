70d4ece3d8856a72a84f720831d79aa6e9b02e4b
cristian <stevanusc00@gmail.com>
2022-07-14 11:59:41 +0700

    new update

---
+++ Golang Dasar/if-else.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main (){
+	name:= "Stevanus"
+	
+	if name == "Cristian" {
+		fmt.Println("Hallo Cristian")
+	} else if name == "Stevanus" {
+		fmt.Println("Hallo Stevanus")
+	} else {
+		fmt.Println("Hi",name)
+	}
+	
+	// length:= len(name)
+	// if length > 5 {
+	// 	fmt.Println("nama terlalu panjang")
+	// } else {
+	// 	fmt.Println("nama sudah benar")
+	// }
+
+	if length := len(name) ;length > 5 {
+		fmt.Println("nama terlalu panjang")
+	} else {
+		fmt.Println("nama sudah benar")
+	}
+
+
+}
